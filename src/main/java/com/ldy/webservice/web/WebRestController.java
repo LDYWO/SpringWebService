fb8b00e3fc0c4d9f41dd050293234225c2452310
Dongyeong Lee <ehrrnno17@naver.com>
2018-07-20 16:57:55 +0900

    20180720 Spring Web Service // AssertError 수정

--- src/main/java/com/ldy/webservice/web/WebRestController.java
+++ src/main/java/com/ldy/webservice/web/WebRestController.java
@@ -1,18 +1,29 @@
 package com.ldy.webservice.web;
 
+import com.ldy.webservice.domain.posts.PostsRepository;
+import com.ldy.webservice.domain.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello") // 매핑 주소
+
     public String hello() {
         return "HelloWorld";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
 }
 
 // @RestController --> @ResponseBody를 모든 메소드에서 적용해준다.
